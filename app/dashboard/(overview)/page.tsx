5a12936c64ef7224a5b84e2b3bda6e5f893760d1
Mustafa Hussain Zariwala <mustafazariwala5253@gmail.com>
2024-03-10 17:33:25 +1100

    Course complete

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -7,6 +7,11 @@ import { fetchLatestInvoices, fetchCardData } from '@/app/lib/data';
 import { Suspense } from 'react';
 import { RevenueChartSkeleton, LatestInvoicesSkeleton, CardsSkeleton} from '@/app/ui/skeletons';
 
+import { Metadata } from 'next';
+export const metadata: Metadata = {
+  title: 'Dashboard',
+};
+
 export default async function Page() {
   const latestInvoices = await fetchLatestInvoices();
   const {totalPaidInvoices, totalPendingInvoices, numberOfInvoices, numberOfCustomers} = await fetchCardData();
